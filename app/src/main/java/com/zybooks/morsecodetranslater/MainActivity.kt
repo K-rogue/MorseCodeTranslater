1c7fc2e46c7f89363b7bd46f0100d7104824e2f9
K-rogue <89587119+K-rogue@users.noreply.github.com>
2023-09-08 11:15:25 -0400

    Final

--- app/src/main/java/com/zybooks/morsecodetranslater/MainActivity.kt
+++ app/src/main/java/com/zybooks/morsecodetranslater/MainActivity.kt
@@ -15,12 +15,16 @@ import android.widget.Switch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vibrator: Vibrator // Var with vibrator property
-    private val timescale = 300L // 1 time unit is set to 300 ms
+    private val timescale = 300L
+    var lastButtonPressTime = 0L
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var englishTextChangedByCode = false
+        var morseTextChangedByCode = false
 
         // Variable for widgets objects
         val outputButton: Button = findViewById(R.id.outputButton)
@@ -39,16 +43,36 @@ class MainActivity : AppCompatActivity() {
         // Listener for regular clicks (short presses)
         buttonEnterMorse.setOnClickListener {
             // Code to execute for a short press
+            val currentTime = System.currentTimeMillis()
+            val timeDifference = currentTime - lastButtonPressTime
+            if (timeDifference > 6000) {
+                editTextMorse.append("   ") // Add space for long press
+            }else if(timeDifference > 3000) {
+                editTextMorse.append(" ")
+            }
+                editTextMorse.append(".") // Append dot for short press
 
+
+            lastButtonPressTime = currentTime
         }
 
         // Listener for long presses
         buttonEnterMorse.setOnLongClickListener {
-            // Code to execute for a long press
+            // Code to execute for a short press
+            val currentTime = System.currentTimeMillis()
+            val timeDifference = currentTime - lastButtonPressTime
+
+            if (timeDifference > 6000) {
+                editTextMorse.append("   ") // Add space for long press
+            }else if(timeDifference > 3000) {
+                editTextMorse.append(" ")
+            }
+            editTextMorse.append("-") // Append dot for short press
 
 
             true // Return true to indicate that the long press was handled
         }
+
 
         vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
@@ -61,27 +85,194 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
         editTextEnglish.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
             override fun afterTextChanged(s: Editable?) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Code to execute when the English text changes
-                editTextMorse.setText(translate(editTextEnglish.getText().toString()))
+                if (!englishTextChangedByCode) {
+                    morseTextChangedByCode = true
+                    editTextMorse.setText(translate(s.toString()))
+                    morseTextChangedByCode = false
+                }
             }
         })
 
         editTextMorse.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
             override fun afterTextChanged(s: Editable?) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Code to execute when the Morse text changes
-
+                if (!morseTextChangedByCode) {
+                    englishTextChangedByCode = true
+                    editTextEnglish.setText(toEnglish(s.toString()))
+                    englishTextChangedByCode = false
+                }
             }
         })
 
 
+
+    }
+    /**
+    Function to turn morse code into english
+    inputs string of . - ' '
+    outputs string of a-z, 0-9
+     */
+    fun toEnglish(morse : String): String {
+        var lastSpace : Int = 0 //int variable to keep track of index of last space indicating new lette/word
+        var currentString : String = "" //used to keep track of the substring of the morse string between current index and last space
+        var english : String = "" // string that will be used for output in english
+        var i : Int = 0 // counter variable
+        //while i is less than the length of the morse string
+        while(i < morse.length){
+            // if i is not the same as the size of the string, other statements check i+1
+            if(i == morse.length-1){
+                //update current substring
+                currentString = morse.substring(lastSpace,i)
+                //translate substring
+                when(currentString){
+                    ".-"-> english += 'a'
+                    "-..."-> english += 'b'
+                    "-.-."-> english += 'c'
+                    "-.."-> english += 'd'
+                    "."-> english += 'e'
+                    "..-."-> english += 'f'
+                    "--."-> english += 'g'
+                    "...."-> english += 'h'
+                    ".."-> english += 'i'
+                    ".---"-> english += 'j'
+                    "-.-"-> english += 'k'
+                    ".-.."-> english += 'l'
+                    "--"-> english += 'm'
+                    "-."-> english += 'n'
+                    "---"-> english += 'o'
+                    ".--."-> english += 'p'
+                    "--.-"-> english += 'q'
+                    ".-."-> english += 'r'
+                    "..."-> english += 's'
+                    "-"-> english +='t'
+                    "..-"-> english += 'u'
+                    "...-"-> english += 'v'
+                    ".--"-> english += 'w'
+                    "-..-"-> english += 'x'
+                    "-.--"-> english +='y'
+                    "--.."-> english += 'z'
+                    "-----"-> english += '0'
+                    ".----"-> english += '1'
+                    "..---"-> english += '2'
+                    "...--"-> english += '3'
+                    "....-"-> english += '4'
+                    "....."-> english += '5'
+                    "-...."-> english += '6'
+                    "--..."-> english += '7'
+                    "---.."-> english += '8'
+                    "----."-> english += '9'
+                }
+            }
+            // else if current and next index are spaces, indicating new word
+            else if(morse[i] == ' ' && morse[i+1] == ' '){
+                //update substring
+                currentString = morse.substring(lastSpace,i)
+                //translate
+                when(currentString){
+                    ".-"-> english += 'a'
+                    "-..."-> english += 'b'
+                    "-.-."-> english += 'c'
+                    "-.."-> english += 'd'
+                    "."-> english += 'e'
+                    "..-."-> english += 'f'
+                    "--."-> english += 'g'
+                    "...."-> english += 'h'
+                    ".."-> english += 'i'
+                    ".---"-> english += 'j'
+                    "-.-"-> english += 'k'
+                    ".-.."-> english += 'l'
+                    "--"-> english += 'm'
+                    "-."-> english += 'n'
+                    "---"-> english += 'o'
+                    ".--."-> english += 'p'
+                    "--.-"-> english += 'q'
+                    ".-."-> english += 'r'
+                    "..."-> english += 's'
+                    "-"-> english +='t'
+                    "..-"-> english += 'u'
+                    "...-"-> english += 'v'
+                    ".--"-> english += 'w'
+                    "-..-"-> english += 'x'
+                    "-.--"-> english +='y'
+                    "--.."-> english += 'z'
+                    "-----"-> english += '0'
+                    ".----"-> english += '1'
+                    "..---"-> english += '2'
+                    "...--"-> english += '3'
+                    "....-"-> english += '4'
+                    "....."-> english += '5'
+                    "-...."-> english += '6'
+                    "--..."-> english += '7'
+                    "---.."-> english += '8'
+                    "----."-> english += '9'
+                }
+                //add space due to new word
+                english += " "
+                //update last space to skip the second of the double spaces
+                lastSpace = i+1
+
+            }
+            //else if index is space and next index is not space, meaning new letter
+            else if(morse[i] == ' ' && morse[i+1] != ' '){
+                //update substring
+                currentString = morse.substring(lastSpace, i)
+                //translate
+                when(currentString){
+                    ".-"-> english += 'a'
+                    "-..."-> english += 'b'
+                    "-.-."-> english += 'c'
+                    "-.."-> english += 'd'
+                    "."-> english += 'e'
+                    "..-."-> english += 'f'
+                    "--."-> english += 'g'
+                    "...."-> english += 'h'
+                    ".."-> english += 'i'
+                    ".---"-> english += 'j'
+                    "-.-"-> english += 'k'
+                    ".-.."-> english += 'l'
+                    "--"-> english += 'm'
+                    "-."-> english += 'n'
+                    "---"-> english += 'o'
+                    ".--."-> english += 'p'
+                    "--.-"-> english += 'q'
+                    ".-."-> english += 'r'
+                    "..."-> english += 's'
+                    "-"-> english +='t'
+                    "..-"-> english += 'u'
+                    "...-"-> english += 'v'
+                    ".--"-> english += 'w'
+                    "-..-"-> english += 'x'
+                    "-.--"-> english +='y'
+                    "--.."-> english += 'z'
+                    "-----"-> english += '0'
+                    ".----"-> english += '1'
+                    "..---"-> english += '2'
+                    "...--"-> english += '3'
+                    "....-"-> english += '4'
+                    "....."-> english += '5'
+                    "-...."-> english += '6'
+                    "--..."-> english += '7'
+                    "---.."-> english += '8'
+                    "----."-> english += '9'
+                }
+                //update last space
+                lastSpace = i+1
+            }
+            //increment
+            i++
+        }
+        //return string in english
+        return english
     }
     /**
     Function for inputting a string of english and outputting a string of morse code. One pause is used for new letter, two for new word
